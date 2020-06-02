44805433adbd382f6cd44df667fe7d4610ee9ccd
ezy820 <ezy820@users.noreply.github.com>
2020-06-02 16:59:09 +0200

    db
    Nastavené ukladanie do databazy spolu s ukladanim do suboru

--- zad/appzz.py
+++ zad/appzz.py
@@ -5,10 +5,22 @@ import time
 import random
 import math
 import serial
+import ConfigParser
+import MySQLdb
+
+
 
 async_mode = None
 
 app = Flask(__name__)
+
+config = ConfigParser.ConfigParser()
+config.read('config.cfg')
+myhost = config.get('mysqlDB', 'host')
+myuser = config.get('mysqlDB', 'user')
+mypasswd = config.get('mysqlDB', 'passwd')
+mydb = config.get('mysqlDB', 'db')
+print myhost
 
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode=async_mode)
@@ -20,19 +32,23 @@ ser.baudrate=9600
 def background_thread(args):
     count = 0    
     i = 0
+    dataCounter =0
     dataList = []
+
     while True:
         ser.write(str(int(9)))
         socketio.sleep(0.5)
         
         count += 1
-       
-        #btnV = dict(args).get('btn_value')
+        dataCounter +=1
+        #dbV = dict(args).get('db_value')
+        #print dbV
+        
         if dict(args).get('A') is not None:
             A = dict(args).get('A')
             A = int(A)
-            print A
-        
+            #print A
+            dbV = dict(args).get('db_value')
           #sliderV = dict(args).get('slider_value')
         if A>0:
             
@@ -42,7 +58,7 @@ def background_thread(args):
           #sliderV = 0
                 ser.write(str(A))
                 i=A
-                print i
+                #print i
             t=time.time()
             read_ser=ser.readline()
             prem=read_ser.split(',')
@@ -57,20 +73,59 @@ def background_thread(args):
             if len(dataList)>0:
             
             #print str(dataList)
-                print(count)
-                print(prem[0])
-                print(prem[1])
-                print(A)
-                
+                #print(count)
+                #print(prem[0])
+                #print(prem[1])
+                #print(A)
+                #print(dbV)
             
                 socketio.emit('my_response',
                     {'data': prem[0], 'data2': prem[1], 'count': count, 'time': t},
                       namespace='/test')
 
+
 @app.route('/')
 def index():
     return render_template('index1.html', async_mode=socketio.async_mode)
       
+@app.route('/graph', methods=['GET', 'POST'])
+def graph():
+    return render_template('graph.html', async_mode=socketio.async_mode)
+    
+@app.route('/db', methods=['GET', 'POST'])
+def db():
+    db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)
+#   cursor = db.cursor()
+#   cursor.execute('''SELECT  hodnoty FROM  graph WHERE id=6''')
+#   rv = cursor.fetchall()
+#   return str(rv)
+    print request.form["data"]
+    fuj = str(request.form["data"])
+    cursor = db.cursor()
+
+    cursor.execute("INSERT INTO zadanie (hodnoty) VALUES (%s)", [fuj])
+    db.commit()
+    fo = open("static/test.txt","a+")
+    fo.write("%s\r\n" %fuj)
+    db.close()
+    return jsonify("ok")
+
+@app.route('/dbdata/<string:num>', methods=['GET', 'POST'])
+def dbdata(num):
+  db = MySQLdb.connect(host=myhost,user=myuser,passwd=mypasswd,db=mydb)
+  cursor = db.cursor()
+  print num
+  cursor.execute("SELECT hodnoty FROM  graph WHERE id=%s", num)
+  rv = cursor.fetchone()
+  return str(rv[0])
+
+
+@app.route('/read/<string:num>', methods=['GET', 'POST'])
+def readmyfile(num):
+    fo = open("static/test.txt","r")
+    rows = fo.readlines()
+    return rows[int(num)-1]
+
 @socketio.on('my_event', namespace='/test')
 def test_message(message):   
     session['receive_count'] = session.get('receive_count', 0) + 1 
